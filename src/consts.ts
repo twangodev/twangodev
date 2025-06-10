78a68db807ec7f618c20eae547fe8c5365db9292
James Ding <jamesding365@gmail.com>
2025-06-09 20:29:45 -0700

    Update consts.ts to refine site description for clarity

--- src/consts.ts
+++ src/consts.ts
@@ -3,8 +3,7 @@ import type { Site, Page, Links, Socials } from "@types";
 // Global
 export const SITE: Site = {
   TITLE: "twango.dev",
-  DESCRIPTION:
-    "Welcome to Astro Sphere, a portfolio and blog for designers and developers.",
+  DESCRIPTION: "James Ding's portfolio, blog, and projects.",
   AUTHOR: "James Ding",
 };
 
